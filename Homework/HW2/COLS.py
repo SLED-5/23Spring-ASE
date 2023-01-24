e49220b304248df89a5afb721a2f830595bd5690
yangzhijin4 <zhijinyang440@gmail.com>
2023-01-23 21:36:46 -0500

    add COLS

--- Homework/HW2/COLS.py
+++ Homework/HW2/COLS.py
@@ -8,7 +8,15 @@ class COLS:
             num = NUM(n, s)
             sym = SYM(n, s)
             col = s.find("^[A-Z]+") and num or sym
+            # push(self.all, col)
+            if not s.find("X$"):
+                if s.find("!$"):
+                    self.klass = col
+                    # push(s.find("[!+-]" and self.y or self.x), col)
 
-
+    def add(self, row):
+        for t in {self.x, self.y}.values():
+            for col in t.values():
+                # col:add()
 
 
