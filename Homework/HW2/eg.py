3944002dfaef077baed8944fdc9f614df6dce359
Gabriel Zhang <1gabriel.zhang1@gmail.com>
2023-01-24 00:32:35 -0500

    Fix some errors

--- Homework/HW2/eg.py
+++ Homework/HW2/eg.py
@@ -1,5 +1,24 @@
 import utils, NUM, SYM, COLS, ROW, DATA
 
+egs = {}
+
+the = {}
+help = '''[[   
+data.lua : an example csv reader script
+(c)2022, Tim Menzies <timm@ieee.org>, BSD-2 
+
+USAGE:   data.lua  [OPTIONS] [-g ACTION]
+
+OPTIONS:
+	-d  --dump  on crash, dump stack = false
+	-f  --file  name of file         = ../etc/data/auto93.csv
+	-g  --go    start-up action      = data
+	-h  --help  show help            = false
+	-s  --seed  random number seed   = 937162211
+
+ACTIONS:
+]]'''
+
 def eg(key, str, fun):
 	global help
 	global egs
@@ -36,9 +55,10 @@ def ssfd():
 		print(k, "mid", utils.o(data.stats("mid", cols, 2)))
 		print("", "div", utils.o(data.stats("div", cols, 2)))
 
-def sum_test():
+def runTest():
 	eg ('the', "show settings", packedOO)
 	eg("csv","read from csv", crfc)
 	eg("data","read DATA csv", drdc)
 	eg ('sym', "check syms", symEgFunc)
 	eg('num', "check nums", numEgFunc)
+	
--- Homework/HW2/script.py
+++ Homework/HW2/script.py
@@ -12,23 +12,6 @@ import config
 import getopt
 import eg
 
-the = {}
-help = '''[[   
-data.lua : an example csv reader script
-(c)2022, Tim Menzies <timm@ieee.org>, BSD-2 
-
-USAGE:   data.lua  [OPTIONS] [-g ACTION]
-
-OPTIONS:
-	-d  --dump  on crash, dump stack = false
-	-f  --file  name of file         = ../etc/data/auto93.csv
-	-g  --go    start-up action      = data
-	-h  --help  show help            = false
-	-s  --seed  random number seed   = 937162211
-
-ACTIONS:
-]]'''
-
 def main(options, help):
 	eg.runTest()
 	egs = eg.egs
@@ -52,4 +35,4 @@ def main(options, help):
 				else:
 					print("✅ pass:" + what[0])
 					
-main(the, help)
+main(eg.the, eg.help)
