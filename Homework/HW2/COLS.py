2ab0c65cf8f83a9681fa248caedee7416112f2e2
yangzhijin4 <zhijinyang440@gmail.com>
2023-01-23 22:40:24 -0500

    add DATA and update COLS

--- Homework/HW2/COLS.py
+++ Homework/HW2/COLS.py
@@ -1,24 +1,25 @@
 from NUM import *
 from SYM import *
-from ROW import *
+import utils
+
 class COLS:
 
-    def __init__(self, t, col, cols):
+    def __init__(self, t, cols):
+        col = {}
         self.name, self.all, self.x, self.y, self.klass = t, {}, {}, {}
         for n,s in enumerate(t):
             num = NUM(n, s)
             sym = SYM(n, s)
             col = s.find("^[A-Z]+") and num or sym
-            # push(self.all, col)
+            utils.push(self.all, col)
             if not s.find("X$"):
                 if s.find("!$"):
                     self.klass = col
-                    # push(s.find("[!+-]" and self.y or self.x), col)
+                    utils.push(s.find("[!+-]" and self.y or self.x), col)
 
     def add(self, row):
         for t in {self.x, self.y}.values():
             for col in t.values():
-                return col
-                # col:add()
+                return col.add(row.cells[col.at])
 
 
---
+++ Homework/HW2/DATA.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from COLS import *
+import utils
+import ROW
+import COLS
+class DATA:
+    def __init__(self, src, fun):
+        self.rows, self.cols = {}, None
+        if type(src) == "string":
+            pd.read_csv(src, self.add())
+        else:
+            utils.fMap(src or {}, fun)
+
+
+    def add(self, t):
+        if self.cols:
+            row_t = ROW()
+            t = t.cells and t and row_t
+            utils.push(self.rows, t)
+        else:
+            self.cols = COLS(t)
+
+
+    def clone(self, init):
+        data = DATA({self.cols.names})
+        x = self.fun1(data)
+        utils.fMap(init or {}, x)
+        return data
+
+    def fun1(self, x):
+        return self.add(x)
+
+    def stats(self, what, cols, n):
+        fun = {}
+        # function fun(k, col) return col:rnd(getmetatable(col)[what or "mid"](col), nPlaces), col.txt end
+        return utils.fKap(cols or self.cols.y, fun)
+
+    def fun2(self, k, col):
+        return 1
+
+
